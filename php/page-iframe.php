895b333c5b2d0e8034e4532414fb70e2c184e3a8
Andreas Grasser <andi@grasserisen.de>
2025-06-24 21:53:11 +0200

    Changed window content css

--- php/page-iframe.php
+++ php/page-iframe.php
@@ -5,6 +5,16 @@
 	<style>
 	body {
 	font-size: 130%;
+	font-family: Georgia, 'Times New Roman', Times, serif;
+	}
+	h1 {
+	font-size: 2rem;
+	}
+	h2 {
+	font-size: 1.6rem;
+	}
+	h3,h4,h5 {
+	font-size: 1.4rem;
 	}
 	</style>
 	<meta charset="utf-8">
